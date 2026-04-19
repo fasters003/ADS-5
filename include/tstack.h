c79c04ef30d4ade74bbb957bdeec5827cea37d5a
fasters003 <andrey1990ver@gmail.com>
2026-04-19 18:59:13 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,54 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+  T* data;
+  int top_index;
+
+ public:
+  TStack() : top_index(-1) {
+    data = new T[size];
+  }
+
+  ~TStack() {
+    delete[] data;
+  }
+
+  bool isEmpty() const {
+    return top_index == -1;
+  }
+
+  bool isFull() const {
+    return top_index == size - 1;
+  }
+
+  void push(T value) {
+    if (!isFull()) {
+      data[++top_index] = value;
+    }
+  }
+
+  T pop() {
+    if (!isEmpty()) {
+      return data[top_index--];
+    }
+    return T();
+  }
+
+  T top() const {
+    if (!isEmpty()) {
+      return data[top_index];
+    }
+    return T();
+  }
+
+  void clear() {
+    top_index = -1;
+  }
+
+  int getSize() const {
+    return top_index + 1;
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
