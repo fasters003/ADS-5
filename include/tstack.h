3f3a326a5726446c338f2683e372a5db6a3261f8
fasters003 <andrey1990ver@gmail.com>
2026-06-02 15:28:55 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,54 +4,38 @@
 
 template<typename T, int size>
 class TStack {
- private:
-  T* data;
-  int top_index;
+private:
+    T arr[size];
+    int top_index;
 
- public:
-  TStack() : top_index(-1) {
-    data = new T[size];
-  }
-
-  ~TStack() {
-    delete[] data;
-  }
-
-  bool isEmpty() const {
-    return top_index == -1;
-  }
-
-  bool isFull() const {
-    return top_index == size - 1;
-  }
-
-  void push(T value) {
-    if (!isFull()) {
-      data[++top_index] = value;
+public:
+    TStack() {
+        top_index = -1;
     }
-  }
 
-  T pop() {
-    if (!isEmpty()) {
-      return data[top_index--];
+    void push(const T& item) {
+        if (top_index < size - 1) {
+            arr[++top_index] = item;
+        }
     }
-    return T();
-  }
 
-  T top() const {
-    if (!isEmpty()) {
-      return data[top_index];
+    T pop() {
+        if (top_index >= 0) {
+            return arr[top_index--];
+        }
+        return T();
     }
-    return T();
-  }
 
-  void clear() {
-    top_index = -1;
-  }
+    T get() const {
+        if (top_index >= 0) {
+            return arr[top_index];
+        }
+        return T();
+    }
 
-  int getSize() const {
-    return top_index + 1;
-  }
+    bool isEmpty() const {
+        return top_index == -1;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
